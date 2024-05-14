463647f6615a62a46d335f57c648e29637203b47
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-14 19:52:17 +0200

    aggiunto ciclo for che controlla la presenza di cocomero nell'array
    creata istruzione condizionale che stampa risposta nella console

--- array-lista/script.js
+++ array-lista/script.js
@@ -1,8 +1,19 @@
-const frutti=["banana", "mela", "pera", "ciliegia", "arancia", "mandarino", "cocomero", "limone", "fragola" ];
+const frutti=["banana", "mela", "pera", "ciliegia", "arancia", "mandarino", "cocomero", "limone", "fragola"];
 
 let pesca=("pesca");
 
 frutti.push(pesca);
+
+for (index = 0; index < frutti.length; index++){
+    
+    if (frutti[index]==["cocomero"]){
+
+        console.log("Trovato! Devo solo preparare il cocktail.");
+
+    }else (frutti[index] != ["cocomero"])
+
+        console.log("Oh no, devo uscire a comprare il cocomero!");
+}
 
 
 
