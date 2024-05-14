1585d7d35c494a5f41e6a46bbb2e8c4e77c86e1f
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-14 21:12:58 +0200

    aggiunta istruzione che non permette che il numero sia minore di 6

--- array_tail/script.js
+++ array_tail/script.js
@@ -1,7 +1,13 @@
 const numeri=[];
 let numeriCasuali=Number.parseInt(prompt("quanti numeri devono essere presenti nel array?"),10);
 
+if (numeriCasuali < 6){
+    numeriCasuali=10
+}
 
+
+
+    
  
 
   
