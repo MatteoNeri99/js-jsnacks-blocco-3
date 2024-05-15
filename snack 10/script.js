878ca812a01bd168389a824c3f70b9d9cd7c078e
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-15 12:20:35 +0200

    esercizio 10

---
+++ snack 10/script.js
@@ -0,0 +1,12 @@
+// Crea due array che hanno un numero di elementi diversi. 
+// Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+const numeriA=[1,2,3,4,5,6,7,8,9,0,];
+
+const numeriB=[1,2,3,4];
+
+while(numeriB.length <= numeriA.length - 1)[
+    numeriB.push(Math.floor(Math.random()*10))
+]
+
+console.log (numeriA, numeriB);
