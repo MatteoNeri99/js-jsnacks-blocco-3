3c0f2b38bbd569bfe59953761f8c92e89ddd6676
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-16 11:40:04 +0200

    snack 12

---
+++ snack 12/script.js
@@ -0,0 +1,27 @@
+// Snack12
+// Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+function parole(parolaUno, parolaDue){
+
+
+    if (parolaUno.length < parolaDue.length){
+
+        console.log(parolaDue);
+
+    }else if (parolaUno.length > parolaDue.length){
+
+        console.log(parolaUno);
+    }else
+
+        console.log(parolaUno, parolaDue);
+        
+
+}
+
+parole("maggio","febbraio")
+
+let paroleUtente=prompt("inserisci una parola");
+
+let paroleUtenteDue=prompt("inserisci una parola");
+
+parole(paroleUtente, paroleUtenteDue)
